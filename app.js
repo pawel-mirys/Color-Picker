58ac28e75bf1627b53df607e13a3989e86390680
YoshiCity <pawel.mirys@gmail.com>
2020-07-30 17:55:09 +0200

    Sliders bg color

--- app.js
+++ app.js
@@ -25,12 +25,36 @@ function randomColors() {
         hexText.innerText = randomColor;
         // Contrast check
         checkTextContrast(randomColor, hexText);
+        // Initial Colorize Sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
 }
-
+// Constrast Check
 function checkTextContrast(color, text) {
     const luminance = chroma(color).luminance();
     luminance > 0.5 ? text.style.color = "black" : text.style.color = "white";
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    // Scale Saturation
+    const noSat = color.set('hsl.s', 0);
+    const fullSat = color.set('hsl.s', 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    // Scale brightness
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBright = chroma.scale(['black', midBright, 'white']);
+    // Update Input Colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)},${scaleBright(0.5)}, ${scaleBright(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(255, 0, 0), rgb(255,255 ,0),rgb(0, 255, 0),rgb(0, 255, 255),rgb(0,0,255),rgb(255,0,255),rgb(255,0,0))`;
 
 }
+
+
 randomColors();
